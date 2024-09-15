5e416a8c19611d9ff62dbdbdcc8ca7c1b163d53b
oculo0204 <96184457+oculo0204@users.noreply.github.com>
2024-09-16 05:42:34 +0900

    feat: 로그인, 회원가입 post 구현

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import IconBack from '../../assets/img/back_btn/Icon_back.svg';
+import axios from 'axios';
 
 const Login = () => {
-    const handleLogin = () => {
-        document.querySelector('.login-form').requestSubmit(); // 폼 제출
+
+    const [id, setId] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+
+    const handleLogin = async() => {
+        try {
+            const response = await axios.post('http://beancp.com:8082/login/login', {
+                id,
+                password
+            });
+            console.log("로그인 성공");
+            navigate('/mainLogin');
+        } catch (err) {
+            console.log("로그인 실패", err);
+        }
     }
 
     return (
@@ -18,11 +34,11 @@ const Login = () => {
                 <form className='login-form'>
                     <div className='input-group'>
                         <label htmlFor='userId'>아이디</label>
-                        <input type='text' id='userId' name='userId' placeholder='아이디를 입력하세요' />
+                        <input type='text' id='userId' name='userId' placeholder='아이디를 입력하세요' value={id} onChange={(e)=>setId(e.target.value)}/>
                     </div>
                     <div className='input-group'>
                         <label htmlFor='password'>비밀번호</label>
-                        <input type='password' id='password' name='password' placeholder='비밀번호를 입력하세요' />
+                        <input type='password' id='password' name='password' placeholder='비밀번호를 입력하세요' value={password} onChange={(e)=>setPassword(e.target.value)}/>
                     </div>
                 </form>
             </div>
